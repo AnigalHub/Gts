51e4d888950e2f757e6cb2a3584a86c740006cec
AnigalHub <anigal1997@gmail.com>
2020-12-07 16:39:08 +0300

    изменен вывод карты и как добраться - с помощью import-a json-файла

--- src/components/contacts.vue
+++ src/components/contacts.vue
@@ -2,13 +2,13 @@
     <div id="contacts">
         <b-container>
             <b-row>
-                <b-col >
+                <b-col>
                     <h2>{{heading}}</h2>
-                    <div class="text" v-html="how_to_get_there"></div>
+                    <div class="text" v-html="address.descr_address"></div>
                 </b-col>
                 <b-col cols="7">
-                    <div class="feature map" id="map-wrap">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2250.3635206939343!2d37.872211516086224!3d55.665278506206185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab7cdc3b33969%3A0xa749f6d233ab0d29!2z0JPQvtGB0KLQtdGF0L3QvtCh0LXRgNCy0LjRgQ!5e0!3m2!1sru!2sru!4v1588940539751!5m2!1sru!2sru"  frameborder="0" allowfullscreen="" aria-hidden="false" ></iframe>
+                    <div class="feature map">
+                        <iframe :src="address.map"></iframe>
                     </div>
                 </b-col>
             </b-row>
@@ -17,13 +17,13 @@
 </template>
 
 <script>
+    import address from '../../public/documents/address';
     export default {
         name: "contacts",
         data(){
             return {
                 heading: 'Контакты',
-                how_to_get_there:`Приходите к нам в офис, обсудим Ваши проекты и предложим варианты их решения. Мы рады каждому Клиенту! </br></br> Наш офис находится в 15 минутах от метро.
-               </br></br> Новорязанское шоссе, дом 6. Длинное кирпичное здание, подъезд 9-10, этаж 2.`
+                address:address,
             }
         }
     }
