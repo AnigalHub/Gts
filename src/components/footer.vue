d9305b08a7385fa0de6735e3f2540657e1ec1279
AnigalHub <anigal1997@gmail.com>
2020-12-07 16:37:40 +0300

    изменен вывод метро, времени работы, адреса, телефона и почты - с помощью json-файла + убраны лишние стили у иконки с адресом

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,29 +3,15 @@
         <b-container>
             <b-row>
                 <b-col>
-                    <div class="text">
-                        <i class="fa fa-train" aria-hidden="true"></i>{{metro}}
-                    </div>
-                    <div class="text">
-                        <i class="fa fa-clock-o" aria-hidden="true"></i> {{working_hours}}
-                    </div>
+                    <div class="text"><i class="fa fa-train" aria-hidden="true"></i>{{address.metro}}</div>
+                    <div class="text"><i class="fa fa-clock-o" aria-hidden="true"></i> {{address.working_hours}}</div>
                 </b-col>
                 <b-col>
-                    <div class="text">
-                        <div v-html="address"></div>
-                    </div>
+                    <div class="text"><div><i class="fa fa-map-marker" aria-hidden="true"></i><span v-html="address.address"></span></div></div>
                 </b-col>
                 <b-col>
-                    <div class="text">
-                        <a :href="link_phone">
-                            <i class="fa fa-phone"></i>{{phone}}
-                        </a>
-                    </div>
-                    <div class="text">
-                        <a :href="link_mail">
-                            <i class="fa fa-envelope-o" aria-hidden="true"></i>{{mail}}
-                        </a>
-                    </div>
+                    <div class="text"><a :href="'tel:' + address.phone"><i class="fa fa-phone"></i>{{address.phone}}</a></div>
+                    <div class="text"><a :href="'mailto:'+ address.mail"><i class="fa fa-envelope-o" aria-hidden="true"></i>{{address.mail}}</a></div>
                 </b-col>
             </b-row>
         </b-container>
@@ -33,18 +19,13 @@
 </template>
 
 <script>
+    import address from '../../public/documents/address';
     export default {
         name: "MyFooter",
         data(){
             return {
                 heading: 'Контакты',
-                metro:' Метро Котельники',
-                working_hours:'Пн. – Пт.: с 9:00 до 17:00',
-                address:`<i class="fa fa-map-marker" aria-hidden="true"></i>  Московская область,</br> г. Котельники, </br>территория Новорязанское шоссе 6,</br> офис 9-10, этаж 2`,
-                phone:'+7 (495) 369-26-73',
-                link_phone:'tel:+74953692673',
-                mail:'info@gtekhno.ru',
-                link_mail:'mailto:info@gtekhno.ru'
+                address:address,
             }
         }
     }
@@ -58,10 +39,6 @@
     color: white;
     max-height: 100%;
 
-    .fa-map-marker{
-        float: left;
-        text-align: center;
-    }
     .text{
         margin-top: 3%;
         text-align: center;
