cbc901cc6d2509586605c7223d1259cafa534d41
AnigalHub <anigal1997@gmail.com>
2021-04-23 00:21:30 +0300

    удалено лишнее, добавлены комментарии

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="footer">
+    <div id="footer">
         <b-container>
             <b-row>
                 <b-col>
-                    <div class="text"><i class="fa fa-train" aria-hidden="true"></i>{{address.metro}}</div>
-                    <div class="text"><i class="fa fa-clock-o" aria-hidden="true"></i> {{address.working_hours}}</div>
+                    <div class="text">{{address.metro}}</div>
+                    <div class="text">{{address.working_hours}}</div>
                 </b-col>
                 <b-col>
-                    <div class="text"><div><i class="fa fa-map-marker" aria-hidden="true"></i><span v-html="address.address"></span></div></div>
+                    <div class="text" v-html="address.address"></div>
                 </b-col>
                 <b-col>
                     <div class="text"><a :href="'tel:' + address.phone"><i class="fa fa-phone"></i>{{address.phone}}</a></div>
@@ -32,32 +32,28 @@
 </script>
 
 <style lang="scss" scoped>
-.footer{
+#footer{
     background: #100a21;
     padding:2% 2% 2% 2% ;
     height: auto;
     color: white;
     max-height: 100%;
-    .text{
+    .text{ /*текст по компоненту - футеру*/
         margin-top: 3%;
         text-align: center;
     }
-    .col{
+    .col{ /*параметры сетки бутстрап (пункт информации в футере)*/
         text-align: center;
         padding-right: 0.313rem !important;
         padding-left: 0.313rem !important;
     }
-    i{
-        color:#8bcdf5;
-        width: 1.875rem;
-    }
-    a{
-        color: #8bcdf5;
-    }
+    /*ссылки*/
+    a{color: #8bcdf5;}
 }
 @media screen and (max-width: 500px) {
-    .row{
-        flex-direction: column;
+    #footer {
+        /*параметры сетки бутстрап (пункты информации в футере)*/
+        .row {flex-direction: column;}
     }
 }
 </style>
